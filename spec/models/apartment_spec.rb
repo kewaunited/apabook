30fef4c9f945940415c04981840c0ebacbfe5ef5
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2015-07-28 22:35:18 +0200

    Refactor Apartment spec to use expect instead of should

--- spec/models/apartment_spec.rb
+++ spec/models/apartment_spec.rb
@@ -2,26 +2,26 @@ require 'rails_helper'
 
 RSpec.describe Apartment, type: :model do
   it 'has a valid factory' do
-    FactoryGirl.create(:apartment).should be_valid
+    expect(FactoryGirl.create(:apartment)).to be_valid
   end
 
   it 'is invalid without a name' do
-    FactoryGirl.build(:apartment, name: nil).should_not be_valid
+    expect(FactoryGirl.build(:apartment, name: nil)).to_not be_valid
   end
 
   it 'is invalid without a door number' do
-    FactoryGirl.build(:apartment, door_number: nil).should_not be_valid
+    expect(FactoryGirl.build(:apartment, door_number: nil)).to_not be_valid
   end
 
   it 'is invalid without a bedroom count' do
-    FactoryGirl.build(:apartment, bedroom_count: nil).should_not be_valid
+    expect(FactoryGirl.build(:apartment, bedroom_count: nil)).to_not be_valid
   end
 
   it 'is invalid without a bathroom count' do
-    FactoryGirl.build(:apartment, bathroom_count: nil).should_not be_valid
+    expect(FactoryGirl.build(:apartment, bathroom_count: nil)).to_not be_valid
   end
 
   it 'is invalid without a floor number' do
-    FactoryGirl.build(:apartment, floor: nil).should_not be_valid
+    expect(FactoryGirl.build(:apartment, floor: nil)).to_not be_valid
   end
 end
