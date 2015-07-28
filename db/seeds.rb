6172516093d632f58a12ec0e4d008c32e7abfa02
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2015-07-28 20:21:54 +0200

    Add header view partial & apartment index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,4 @@
 #
 # Examples:
 #
-apartments = Apartment.create([{ name: 'Chocolate', door_number: '10', bedroom_count: 1, bathroom_count: 1, floor: 2 }, { name: 'Comfortable', door_number: '15', bedroom_count: 2, bathroom_count: 1, floor: 3 }])
+apartments = Apartment.create([{ name: 'Chocolate', door_number: '10', bedroom_count: 1, bathroom_count: 1, floor: 2 }, { name: 'Comfortable', door_number: '15', bedroom_count: 2, bathroom_count: 1, floor: 3 }, { name: 'Flower', door_number: '22', bedroom_count: 1, bathroom_count: 1, floor: 0 }])
