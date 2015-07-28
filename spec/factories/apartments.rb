0f2b37ea926ff5f4cf7260c0caa6efbfc4e223d2
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2015-07-28 16:19:54 +0200

    Add tests for Apartment model validations

--- spec/factories/apartments.rb
+++ spec/factories/apartments.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :apartment do
-    door_number "MyString"
-bedroom_count 1
-bathroom_count 1
-room_description "MyText"
-floor 1
+    name 'Chocolate'
+    door_number '10'
+    bedroom_count 1
+    bathroom_count 1
+    room_description 'One bedroom apartment with a living room. It has an open-plane kitchen, a bathroom with a shower. Free WiFi Internet is available for free to all our guests.'
+    floor 1
   end
-
 end
--- spec/models/apartment_spec.rb
+++ spec/models/apartment_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Apartment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    FactoryGirl.create(:apartment).should be_valid
+  end
+
+  it 'is invalid without a name' do
+    FactoryGirl.build(:apartment, name: nil).should_not be_valid
+  end
+
+  it 'is invalid without a door number' do
+    FactoryGirl.build(:apartment, door_number: nil).should_not be_valid
+  end
+
+  it 'is invalid without a bedroom count' do
+    FactoryGirl.build(:apartment, bedroom_count: nil).should_not be_valid
+  end
+
+  it 'is invalid without a bathroom count' do
+    FactoryGirl.build(:apartment, bathroom_count: nil).should_not be_valid
+  end
+
+  it 'is invalid without a floor number' do
+    FactoryGirl.build(:apartment, floor: nil).should_not be_valid
+  end
 end
